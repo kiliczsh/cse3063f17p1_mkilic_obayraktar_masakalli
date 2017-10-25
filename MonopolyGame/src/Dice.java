7abfb8f0769b81e69995e52a2245880e4faaee17
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2017-10-25 14:29:20 +0300

    added Dice class
    initial Dice class commit

---
+++ MonopolyGame/src/Dice.java
@@ -0,0 +1,71 @@
+
+/**
+ * @author 
+ *
+ */
+public class Dice {
+	private int faceValue;
+	//private int doubleCounter=0; //in player class
+	public Dice() {
+		
+	}
+	
+	public void rollDice() {
+		int newFaceValue=(int)(Math.random()*6+1);
+		setFaceValue(newFaceValue);
+		System.out.println(newFaceValue);
+	}
+	
+	public void checkDoubleCounter(Player player,Dice dice1,Dice dice2) {
+		int value1,value2;
+		value1=dice1.getFaceValue();
+		value2=dice2.getFaceValue();
+		if(value1==value2) {
+			player.doubleCounter++;
+			if(player.doubleCounter >=3) {
+			//	player.setCurrentPosition("Jail"); // will added soon
+			}
+		}else {
+			Player.doubleCounter=0;
+		}
+	}
+	
+	
+	/**
+	 * @return the faceValue
+	 */
+	public int getFaceValue() {
+		return faceValue;
+	}
+	/**
+	 * @param faceValue the faceValue to set
+	 */
+	public void setFaceValue(int faceValue) {
+		this.faceValue = faceValue;
+	}
+	/**
+	 * @return the doubleCounter
+	 */
+	public int getDoubleCounter() {
+		return doubleCounter;
+	}
+	/**
+	 * @param doubleCounter the doubleCounter to set
+	 */
+	public void setDoubleCounter(int doubleCounter) {
+		
+		this.doubleCounter = doubleCounter;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Dice [faceValue=" + faceValue + ", doubleCounter=" + doubleCounter + ", getFaceValue()="
+				+ getFaceValue() + ", getDoubleCounter()=" + getDoubleCounter() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+	
+}
+
--- MonopolyGame/src/MonopolyGame.java
+++ MonopolyGame/src/MonopolyGame.java
@@ -3,7 +3,14 @@ public class MonopolyGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Mali");
+		System.out.println("Hello Object Oriented");
+		Dice dice1 = new Dice();
+		Dice dice2 = new Dice();
+		dice1.rollDice();
+		dice2.rollDice();
+		System.out.println(""+dice1.toString());
+		System.out.println(""+dice2.toString());
+		
 	}
 
 }
