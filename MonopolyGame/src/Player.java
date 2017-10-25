b3bb33e8633498571de2214cf5c5d3f127485786
mehmetalisakalli <32929663+mehmetalisakalli@users.noreply.github.com>
2017-10-25 15:04:22 +0300

    player

--- MonopolyGame/src/Player.java
+++ MonopolyGame/src/Player.java
@@ -1,4 +1,80 @@
 
 public class Player {
-//new player class
+
+private	int id;
+private	String playercolor;
+private	Square currentposition;
+private	Money money;
+	
+	/**
+ * @return the id
+ */
+public int getId() {
+	return id;
+}
+
+/**
+ * @param id the id to set
+ */
+public void setId(int id) {
+	this.id = id;
+}
+
+/**
+ * @return the playercolor
+ */
+public String getPlayercolor() {
+	return playercolor;
+}
+
+/**
+ * @param playercolor the playercolor to set
+ */
+public void setPlayercolor(String playercolor) {
+	this.playercolor = playercolor;
+}
+
+/**
+ * @return the currentposition
+ */
+public Square getCurrentposition() {
+	return currentposition;
+}
+
+/**
+ * @param currentposition the currentposition to set
+ */
+public void setCurrentposition(Square currentposition) {
+	this.currentposition = currentposition;
+}
+
+/**
+ * @return the money
+ */
+	
+void myTurn() {
+	//deciding player turn
+	
+}
+
+void addPlayer(Player player) {
+	
+	//adding players
+}
+
+void removePlayer(Player player) {
+	
+	//remove players from the game
+	
+   }
+
+/* (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
+@Override
+public String toString() {
+	System.out.println( "Player [id=" + id + ", playercolor=" + playercolor + ", money=" + money + "]");
+	return "Player [id=" + id + ", playercolor=" + playercolor + ", money=" + money + "]";
+}
+
 }
