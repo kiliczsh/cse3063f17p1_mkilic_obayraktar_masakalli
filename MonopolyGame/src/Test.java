36fce66c276939de26b9c76888926b29e409d542
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2017-10-26 12:53:42 +0300

    added test

--- MonopolyGame/src/Test.java
+++ MonopolyGame/src/Test.java
@@ -5,12 +5,10 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
-/**
- * 
- */
+import junit.framework.Assert;
 
 /**
- * @author muham
+ * @author 
  *
  */
 public class Test {
@@ -20,6 +18,7 @@ public class Test {
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("beforeclass");
 	}
 
 	/**
@@ -27,6 +26,7 @@ public class Test {
 	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		 System.out.println("afterclass");
 	}
 
 	/**
@@ -34,6 +34,7 @@ public class Test {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("setup");
 	}
 
 	/**
@@ -41,11 +42,32 @@ public class Test {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		 System.out.println("after");
 	}
 
+	@SuppressWarnings("deprecation")
 	@org.junit.Test
 	public void test() {
-		fail("Not yet implemented");
+		GameBoard gameBoard = new GameBoard();
+		Player player = new Player(100);
+	    player.setPlayerName("TestPlayer");
+	    Dice dice = new Dice();
+	    dice.rollDice();
+	    Square square = new Square(5,"TestSquare");
+	    Assert.assertEquals(100, player.getPlayerID());
+	    Assert.assertEquals("TestPlayer", player.getPlayerName());
+	    Assert.assertTrue(gameBoard.squareList[0].getSquareName().equals(player.getCurrentposition().getSquareName()));
+	    Assert.assertTrue(dice.rollDice()<=6 && dice.rollDice()>=1);
+	    Assert.assertEquals(5, square.getSquareID());
+	    Assert.assertEquals("TestSquare", square.getSquareName());
+	    Assert.assertNotNull(square);
+	    Assert.assertNotNull(dice);
+	    Assert.assertNotNull(player);
+	    Assert.assertNotNull(gameBoard);
+	    
+	    
+	    
+	    
+	    
 	}
-
 }
