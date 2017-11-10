61a41bf6ea11bbae936b6f6716d8f0b92ebc79fd
Muhammed Kılıç <muhammeddkilicc@gmail.com>
2017-11-11 00:27:51 +0300

    Iteration 2 Implementation
    recoded Monopoly and GameBoard classes

--- MonopolyGame/src/GameBoard.java
+++ MonopolyGame/src/GameBoard.java
@@ -1,178 +1,134 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameBoard {
-	//Declarations and Initilizations
-	public static int numOfPlayers;
-	Scanner input = new Scanner(System.in);
-	GameBoard gameBoard;
-	Dice dice1 = new Dice();
-	Dice dice2 = new Dice();
-	Player[] playerList = createPlayers(input,numOfPlayers);
-	Player[] playingOrder = diceTournament(gameBoard,playerList);
-	Square[] squareList= createCells();
-
-	public  Square[] createCells() {
-		//List of Squares
-		ArrayList<Square> squareList = new ArrayList<Square>();
-		Square  square1,square2,square3,square4,square5,square6,square7,square8,square9,square10,
-		square11,square12,square13,square14,square15,square16,square17,square18,square19,square20,
-		square21,square22,square23,square24,square25,square26,square27,square28,square29,square30,
-		square31,square32,square33,square34,square35,square36,square37,square38,square39,square40;
-		squareList.add((square1 = new Square(1,"GO")));
-		squareList.add((square2 = new Square(2,"Property")));
-		squareList.add((square3 = new Square(3,"Community Chest")));
-		squareList.add((square4 = new Square(4,"Property")));
-		squareList.add((square5 = new Square(5,"Income Tax")));//must pay %10 of total
-		squareList.add((square6 = new Square(6,"Railway")));
-		squareList.add((square7 = new Square(7,"Property")));
-		squareList.add((square8 = new Square(8,"Chance")));
-		squareList.add((square9 = new Square(9,"Property")));
-		squareList.add((square10 = new Square(10,"Property")));
-		squareList.add((square11 = new Square(11,"Jail")));// first exit 
-		squareList.add((square12 = new Square(12,"Property")));
-		squareList.add((square13 = new Square(13,"Electric")));
-		squareList.add((square14 = new Square(14,"Property")));
-		squareList.add((square15 = new Square(15,"Property")));
-		squareList.add((square16 = new Square(16,"Railway")));
-		squareList.add((square17 = new Square(17,"Property")));
-		squareList.add((square18 = new Square(18,"Community Chest")));
-		squareList.add((square19 = new Square(19,"Property")));
-		squareList.add((square20 = new Square(20,"Property")));
-		squareList.add((square21 = new Square(21,"Free Parking")));
-		squareList.add((square22 = new Square(22,"Property")));
-		squareList.add((square23 = new Square(23,"Chance")));
-		squareList.add((square24 = new Square(24,"Property")));
-		squareList.add((square25 = new Square(25,"Property")));
-		squareList.add((square26 = new Square(26,"Railway")));
-		squareList.add((square27 = new Square(27,"Property")));
-		squareList.add((square28 = new Square(28,"Property")));
-		squareList.add((square29 = new Square(29,"Water")));
-		squareList.add((square30 = new Square(30,"Property")));
-		squareList.add((square31 = new Square(31,"Go To Jail"))); // go to square 11
-		squareList.add((square32 = new Square(32,"Property")));
-		squareList.add((square33 = new Square(33,"Property")));
-		squareList.add((square34 = new Square(34,"Community Chest)")));
-		squareList.add((square35 = new Square(35,"Property")));
-		squareList.add((square36 = new Square(36,"Railway")));
-		squareList.add((square37 = new Square(37,"Chance")));
-		squareList.add((square38 = new Square(38,"Property")));// Park Place
-		squareList.add((square39 = new Square(39,"Luxury Tax")));
-		squareList.add((square40 = new Square(40,"Property")));//Boardwalk
-		Square[] squareArray = squareList.toArray(new Square[40]);
-		return squareArray;
-	}
-
-	public void move(GameBoard gameBoard,Player player,int turnTotalMovement) {
-		Square currentCell= player.getCurrentposition();
-		int nextCellLoc=currentCell.getSquareID()+turnTotalMovement;
-		if(nextCellLoc>40) {
-			player.increaseMoney(200);
-			nextCellLoc= nextCellLoc%40;
+	int currentTurn = 0;
+	int totalPlayer = 0;
+	Player[] players;
+	Square[] squares = new Square[40];
+	String[] names = new String[] { "Property", "Community Chest", "Railway", "City", "Peace", "Village"};
+	
+	public GameBoard(int totalPlayer) {
+		players = new Player[totalPlayer];
+		this.totalPlayer = totalPlayer;
+		for(int i = 0;i < players.length;i++){
+			players[i] = new Player(i, "Player " + (i + 1));
 		}
-		player.checkGo();
-		if(player.getCurrentposition().getSquareID()==31 || player.getCurrentposition().getSquareID()==11 ) {//Go To Jail
-			player.goToJail();
+		shuffleArray(players);
+		int a =1;
+		System.out.println("Dice Tournament Results");
+		for(Player player:players) {
+			System.out.print(a+": "+player.getName()+"\n");
+			a++;
 		}
-		player.isInJail = player.checkJail();
-		if(player.isInJail) {
-			System.out.println(player.getPlayerName()+" is in Jail now.");
-		}else {
-			player.setCurrentposition(getSquaresByID(nextCellLoc));
-		}
-		
-	}
-
-	public Player[] diceTournament(GameBoard gameBoard,Player[] playerList) {
-		for(int i=0;i<playerList.length;i++) {
-			playerList[i].diceTournamentValue = dice1.rollDice() + dice2.rollDice();
-			for (Player player : playerList) {
-				   for(int j=0;j<playerList.length;j++){
-					   if(player.getPlayerID() != playerList[i].getPlayerID()) {
-						   while(playerList[i].diceTournamentValue == player.diceTournamentValue) {
-							   playerList[i].diceTournamentValue = dice1.rollDice() + dice2.rollDice();
-						   }
-					   }
-				   }
-			}
-		}		
-		Player[] playingList = new Player[playerList.length];
-		int max=playerList[0].diceTournamentValue;
-		int min=playerList[0].diceTournamentValue;
-		for(int k=0;k<playerList.length;k++) {
-			if(playerList[k].diceTournamentValue>max) {
-				max=playerList[k].diceTournamentValue;
-			}else if(playerList[k].diceTournamentValue<min) {
-				min=playerList[k].diceTournamentValue;
+		System.out.println();
+		Random rand = new Random();
+		for(int i = 0;i < squares.length;i++){
+			if(i == 0){
+				squares[i] = new GoSquare("GO");
+			}else if(i == 5) {
+				squares[i] = new IncomeTaxSquare("Income Tax");
+			}else if(i == 11){
+				squares[i] = new JailSquare("Jail");
+			}else if(i == 21){
+				squares[i] = new FreeParkingSquare("Free Parking");
+			}else if(i == 31){
+				squares[i] = new GoToJailSquare("Go to Jail");
+			}else if(i == 39) {
+				squares[i] = new LuxuryTaxSquare("Luxury Tax");
+			}else{
+				squares[i] = new NormalSquare(names[rand.nextInt(names.length)] + " " + names[rand.nextInt(names.length)], 400 + rand.nextInt(300));
 			}
 		}
-		Integer[][] diceValues= new Integer[playerList.length][2];
-		for(int t=0;t<playerList.length;t++) {
-			diceValues[t][0]=playerList[t].diceTournamentValue;
-			diceValues[t][1]=playerList[t].getPlayerID();
-		}
-		Arrays.sort(diceValues, new ArrayComparator(0, true));
-
-		for(int i=0;i<playerList.length;i++) {
-			for(int j=0;j<diceValues.length;j++) {
-					if( diceValues[j][0] == playerList[i].diceTournamentValue && diceValues[j][1] == playerList[i].getPlayerID()) {
-					playingList[i]=playerList[i];
-				}
-			}
-		}
-		return playingList;
-	}
-
-	public Player[] createPlayers(Scanner input,int numOfPlayers) {
-		ArrayList<Player> playerList = new ArrayList<Player>();
-		int i=0;
-		for(i=1;i<=8;i++) {
-			Player player= new Player(i);
-			playerList.add(player);
-		}
-		Player[] playerArray = playerList.toArray(new Player[8]);
-		return playerArray;
 	}
 	
-	/**
-	 * @param numOfPlayers the numOfPlayers to set
-	 */
-	public static void setNumOfPlayers(int numOfPlayers) {
-		GameBoard.numOfPlayers = numOfPlayers;
-	} 
-
-	/**
-	 * @return the squares
-	 */
-	public ArrayList<Square> getSquares() {
-		return getSquares();
+	public Square movePlayer(Player player, int face) {
+		return movePlayer(player, face, true);
 	}
-
-	public boolean checkDoubleCounter(Player player,int dice1Value, int dice2Value) {
-		int value1=dice1Value;
-		int value2=dice2Value;
-		value1=dice1.getFaceValue();
-		value2=dice2.getFaceValue();
-		if(value1==value2) {
-			player.doubleCounter++;
-			if(player.doubleCounter >=3) {
-				return false;
-			}else {
-				return true;
-			}
-		}else{
-			player.doubleCounter=0;
-			return true;
+	
+	public Square movePlayer(Player player, int face, boolean count) {
+		if(player.isBrokeOut() ){
+			return squares[player.getCurrentPosition()]; 
 		}
-	}
-
-	public Square getSquaresByID(int squareID) {
-		for(Square square : squareList) { 
-			   if( square.getSquareID()==squareID ) { 
-			       return square;
-			   }
+		int newPosition = normalizePosition(player.getCurrentPosition() + face);
+		player.setPosition(newPosition);
+		System.out.println("[Position: " + (player.getCurrentPosition()+1) + "] [Total Money: $" + player.getMoney().getMoney() + "] " +  player.getName() + " goes to " + squares[player.getCurrentPosition()].getName());
+		squares[newPosition].doAction(player, this);
+		if(player.getMoney().isBrokeOut()){
+			System.out.println("\nTurn : "+(player.getTotalWalk() + 1)+ " - "+player.name+"\n");
+			System.out.println("[Position: " + (player.getCurrentPosition()+1) + "] [Total Money: $" + player.getMoney().getMoney() + "] " + player.getName() + " has been broke out!");
+			player.setBrokeOut(true);
+		}else{
+			if(count){
+				player.nextTurn();
 			}
+		}
+		return squares[newPosition];
+	}
+	
+	public boolean hasWinner() {
+		int ingame = 0;
+		for(Player player:players){
+			if(!player.isBrokeOut()){
+				ingame++;
+			}
+		}
+		return ingame <= 1;
+	}
+	
+	public Player getWinner() {
+		if(!hasWinner()){ return null; }
+		for(Player player:players){
+			if(!player.isBrokeOut()){ return player; }
+		}
 		return null;
 	}
+	
+	public Player getMaxMoneyPlayer() {
+		Player maxplayer = null;
+		for(Player player:players){
+			if(maxplayer == null || maxplayer.getMoney().getMoney() < player.getMoney().getMoney()){
+				maxplayer = player;
+			}
+		}
+		return maxplayer;
+	}
+	
+	public int normalizePosition(int position) {
+		return position % squares.length;
+	}
+	
+	public Player getCurrentPlayer() {
+		return players[currentTurn];
+	}
+	
+	public Player[] getPlayers() {
+		return players;
+	}
+	
+	public void nextTurn() {
+		if(++currentTurn >= players.length){
+			currentTurn = 0;
+		}
+	}
+	
+	public Player getPlayer(int id) {
+		return players[id];
+	}
+	
+	public int getTotalSquare() {
+		return squares.length;
+	}
+	
+	public void shuffleArray(Player[] players2)
+	  {
+	    Random rnd = ThreadLocalRandom.current();
+	    for (int i = players2.length - 1; i > 0; i--)
+	    {
+	      int index = rnd.nextInt(i + 1);
+	      Player a = players2[index];
+	      players2[index] = players2[i];
+	      players2[i] = a;
+	    }
+	  }
 }
---
+++ MonopolyGame/src/Monopoly.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Monopoly {
+	Dice die1 = new Dice();
+	Dice die2 = new Dice();
+	GameBoard gameBoard;
+	
+	public Monopoly(int totalPlayer) {
+		gameBoard = new GameBoard(totalPlayer);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("\tMonopoly\n");
+		Scanner scanner = new Scanner(System.in);
+		int totalPlayer = 0;
+		int turnNumber = 10;
+		while (totalPlayer < 2 || totalPlayer > 8) {
+			try {
+				System.out.println("How many people are playing?");
+				System.out.print("Players (2 - 8): ");
+				totalPlayer = scanner.nextInt();
+				System.out.println("Enter number of turns you want to simulate:");
+				turnNumber = scanner.nextInt();
+				if(!(turnNumber>=1 && turnNumber<=100)) {
+					turnNumber=10;
+				}
+				System.out.println(turnNumber+" turns will be simulated...");
+			}
+			catch(Exception e) {
+				System.err.println("Error: Number too large.");
+				continue;
+			}
+			if(totalPlayer > 8) {
+				System.err.println("Error: Invalid player count.");
+			}
+		}
+		scanner.close();
+		Monopoly game = new Monopoly(totalPlayer);
+		game.startGame(turnNumber);
+	}
+	
+	public void startGame(int turnNumber) {
+		System.out.println("----------");
+		
+		while (!isGameEnd(turnNumber) && !gameBoard.hasWinner()){
+			if(!gameBoard.getCurrentPlayer().isBrokeOut()){
+				int face = gameBoard.getCurrentPlayer().tossDie(die1,die2);
+				if(gameBoard.getCurrentPlayer().doubleCounter >= 3) {
+					System.out.println("\nTurn : "+(gameBoard.getCurrentPlayer().getTotalWalk() + 1)+ " - "+gameBoard.getCurrentPlayer().name+"\n");
+					System.out.println("[Position: " + (gameBoard.getCurrentPlayer().getCurrentPosition()+1) + "] [Total Money: $" + gameBoard.getCurrentPlayer().getMoney().getMoney() + "] " + gameBoard.getCurrentPlayer().getName() + " rolled double 3 times.");
+					gameBoard.getCurrentPlayer().setPosition(11);
+					gameBoard.getCurrentPlayer().doubleCounter=0;
+					System.out.println("[Position: " + (gameBoard.getCurrentPlayer().getCurrentPosition()+1) + "] [Total Money: $" + gameBoard.getCurrentPlayer().getMoney().getMoney() + "] " + gameBoard.getCurrentPlayer().getName() + " is in Jail now.");
+				
+				
+				}else {
+					gameBoard.movePlayer(gameBoard.getCurrentPlayer(), face);
+				}
+				
+			}
+			gameBoard.nextTurn();
+		}
+		System.out.println("----------");
+		if(gameBoard.hasWinner()){
+			System.out.println(gameBoard.getWinner().getName() + " is won by don't brokeout!");
+		}else{
+			System.out.println(gameBoard.getMaxMoneyPlayer().getName() + " is won by have most money!");
+		}
+		System.out.println("Game over!");
+	}
+	
+	public boolean isGameEnd(int turnNumber) {
+		for(Player player:gameBoard.getPlayers()){
+			if(player.getTotalWalk() < turnNumber){ return false; }
+		}
+		return true;
+	}
+}
