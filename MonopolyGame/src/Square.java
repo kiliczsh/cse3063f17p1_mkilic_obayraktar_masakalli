fdc37834a75c8c6c3d53c4c8d6e75b36408c9184
onurbayraktar <32903278+onurbayraktar@users.noreply.github.com>
2017-11-25 00:13:58 +0300

    Update square class
    Added new features to Square class

--- MonopolyGame/src/Square.java
+++ MonopolyGame/src/Square.java
@@ -1,14 +1,35 @@
 public abstract class Square {
 	
-	int price;
+	int price=0;
 	String name;
+	Player owner=null;
+	int squareID;
+	int rent;
 	
 	public Square(String name) {
 		this.name = name;
 	}
 	
+	public Square(String name,int squareID) {
+		this.name=name;
+		this.squareID=squareID;
+	}
+	public Square(String name,int squareID,int price) {
+		this.name=name;
+		this.squareID=squareID;
+		this.price=price;
+	}
+	
 	public String getName() {
 		return name;
+	}
+	
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+	
+	public Player getOwner() {
+		return owner;
 	}
 	
 	public abstract void doAction(Player player, GameBoard board);
