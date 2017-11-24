4e3365347c8e41b257aa5a86aaee615611f9f028
onurbayraktar <32903278+onurbayraktar@users.noreply.github.com>
2017-11-25 00:20:02 +0300

    updating normal square
    adding normal square features

--- MonopolyGame/src/NormalSquare.java
+++ MonopolyGame/src/NormalSquare.java
@@ -1,17 +1,13 @@
 import java.util.Random;
 
 public class NormalSquare extends Square {
-	int price;
-	int owner = -1;
+
 	
-	public NormalSquare(String name, int price) {
-		super(name);
-		this.price = price;
+	public NormalSquare(String name,int squareID,int price) {
+		super(name,squareID,price);
 	}
 	
-	public void setOwner(int owner) {
-		this.owner = owner;
-	}
+
 	
 	public int getPrice() {
 		return price;
@@ -19,30 +15,33 @@ public class NormalSquare extends Square {
 	
 	@Override
 	public void doAction(Player player, GameBoard board) {
-		//player.getMoney().substractMoney(1);
-		System.out.println("[Position: " + (player.getCurrentPosition()+1) + "] [Total Money: $" + player.getMoney().getMoney() + "] " + "This is a normal square with no action.");
-		
-		if(owner < 0){
-			//Util.print(player, player.getName() + ", do you want to buy " + getName() + "?");
-			System.out.println(player.getName() + ", do you want to buy " + getName() + "?");
-			Random rand = new Random();
-			if(rand.nextBoolean()){
-				//Util.print(player, player.getName() + " buy " + getName() + " for " + price);
-				System.out.println(player.getName() + " buy " + getName() + " for " + price);
-				owner = player.getID();
-				player.getMoney().substractMoney(price);
-			}else{
-				//Util.print(player, player.getName() + " don't want to buy " + getName());
-				System.out.println(player.getName() + " don't want to buy " + getName());
+		if(owner==null){
+			System.out.println("There is no owner of "+getName()+".");
+			if(player.getMoney().money >= price) {
+				
+				Dice newDice=new Dice();
+				int faceDice=newDice.getFace();
+				if(faceDice>=4){
+					System.out.println("[Dice Value: "+faceDice+"] "+player.getName() + " buy " + getName() + " for " + price);
+					board.squares[player.getCurrentPosition()].owner=player;
+					player.getMoney().substractMoney(price);
+				}else{
+					System.out.println(player.getName() + " don't want to buy " + getName());
+				}
+			}else {
+				System.out.println(player.getName() + " can't buy " + getName());
 			}
-		}else{
-			if(owner != player.getID()){
-				int lost = price * 70 / 100;
-				//Util.print(player, player.getName() + " lost " + lost + " money to " + board.getPlayer(owner).getName());
-				System.out.println(player.getName() + " lost " + lost + " money to " + board.getPlayer(owner).getName());
-				player.getMoney().substractMoney(lost);
-				board.getPlayer(owner).getMoney().addMoney(lost);
+
+		}else{//if there is an owner
+			if(owner.getID()!=player.getID()){
+				rent = price * 70 / 100;
+				System.out.println(player.getName() + " paid $" + rent + " rent to owner " + board.squares[player.getCurrentPosition()].owner.getName()+".");
+				player.getMoney().substractMoney(rent);
+				board.squares[player.getCurrentPosition()].owner.getMoney().addMoney(rent);
 			}
 		}
+	
+	System.out.println(player.getName()+": [Position: " + (player.getCurrentPosition()+1) + "] [Total Money: $" + player.getMoney().getMoney() + "] " );
+
 	}
 }
